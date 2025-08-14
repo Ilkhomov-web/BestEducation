3c9adbcc7acac98a800da6dc1882e372e5bb895d
ilkhomov-web <ilhomovelyor119@gmail.com>
2025-08-14 16:21:48 +0500

    some change

--- src/components/myTeachers/Teacher.jsx
+++ src/components/myTeachers/Teacher.jsx
@@ -1,13 +1,28 @@
+// Teacher.jsx
 import { Box, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "./teacher.css";
 import { Pagination } from "swiper/modules";
 import { teachersData } from "../../data/teacherData";
+import TeacherModal from "./TeacherModal";
 
 const Teacher = () => {
+  const [open, setOpen] = useState(false);
+  const [selectedTeacher, setSelectedTeacher] = useState(null);
+
+  const handleClickOpen = (teacher) => {
+    setSelectedTeacher(teacher);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setSelectedTeacher(null);
+  };
+
   return (
     <Box sx={{ margin: "20px 0px" }}>
       <Swiper
@@ -19,24 +34,37 @@ const Teacher = () => {
         modules={[Pagination]}
         className="mySwiper"
       >
-        {teachersData.map((teacher) => {
-          return (
-            <SwiperSlide style={{ display: "flex", flexDirection: "column" }}>
-              <Box component={"img"} src={teacher.image}></Box>
-              <Typography
-                variant="h6"
-                sx={{
-                  borderLeft: "10px solid #246cb6",
-                  paddingLeft: "10px",
-                  textAlign: "center",
-                }}
-              >
-                {teacher.name}
-              </Typography>
-            </SwiperSlide>
-          );
-        })}
+        {teachersData.map((teacher) => (
+          <SwiperSlide
+            key={teacher.id}
+            onClick={() => handleClickOpen(teacher)}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              cursor: "pointer",
+            }}
+          >
+            <Box component={"img"} src={teacher.image} alt={teacher.name} />
+            <Typography
+              variant="h6"
+              sx={{
+                borderLeft: "10px solid #246cb6",
+                paddingLeft: "10px",
+                textAlign: "center",
+              }}
+            >
+              {teacher.name}
+            </Typography>
+          </SwiperSlide>
+        ))}
       </Swiper>
+
+      {/* Modal faqat bitta joyda turadi */}
+      <TeacherModal
+        handleClose={handleClose}
+        open={open}
+        teacher={selectedTeacher}
+      />
     </Box>
   );
 };
--- src/components/myTeachers/TeacherModal.jsx
+++ src/components/myTeachers/TeacherModal.jsx
@@ -1,7 +1,50 @@
+// TeacherModal.jsx
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
-const TeacherModal = () => {
-  return <div></div>;
+const TeacherModal = ({ handleClose, open, teacher }) => {
+  if (!teacher) return null; // Agar teacher tanlanmagan bo‘lsa, hech narsa ko‘rsatmaymiz
+
+  return (
+    <Box>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        sx={{ display: "flex", justifyContent: "center" }}
+        maxWidth="sm"
+        fullWidth
+      >
+        <Box
+          component={"img"}
+          src={teacher.image}
+          sx={{ width: "300px", height: "300px" }}
+        ></Box>
+        <DialogTitle>{teacher.name}</DialogTitle>
+        <DialogContent>
+          <Typography variant="body1" gutterBottom>
+            <strong>Tajriba:</strong> {teacher.experience}
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            <strong>Yutuqlar:</strong> {teacher.achievements}
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            <strong>O‘quvchilar:</strong> {teacher.students}
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Yopish</Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
+  );
 };
 
 export default TeacherModal;
